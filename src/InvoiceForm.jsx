2cca23aa9b3f8665a2fed39638a15486e71eba0f
kunal_2000 <manohar.shewale77@gmail.com>
2024-09-25 11:39:45 +0530

    The Seller and buyer select code updated

--- src/InvoiceForm.jsx
+++ src/InvoiceForm.jsx
@@ -3,12 +3,22 @@ import { FormControl, Text } from "@chakra-ui/react";
 import parties from "./parties"; // Importing the parties data
 
 const InvoiceForm = () => {
-  const [selectParty, setSelectParty] = useState("");
+  // Seller data
+  const [selectSellerParty, setSelectSellerParty] = useState(null);
 
-  const partySelect = (e) => {
+  const sellerPartySelect = (e) => {
     const selectName = e.target.value;
     const partyDetails = parties.find((party) => party.name === selectName);
-    setSelectParty(partyDetails);
+    setSelectSellerParty(partyDetails);
+  };
+
+  // Buyer data
+  const [selectBuyerParty, setSelectBuyerParty] = useState(null);
+
+  const buyerPartySelect = (e) => {
+    const selectName = e.target.value;
+    const partyDetails = parties.find((party) => party.name === selectName);
+    setSelectBuyerParty(partyDetails);
   };
 
   return (
@@ -16,41 +26,73 @@ const InvoiceForm = () => {
       <FormControl>
         <Text>Seller Information</Text>
         <br />
-        <Text>Firm Name</Text>
-
         <select
-          placeholder="Select Firm Name"
-          value={selectParty?.name || ""}
-          onChange={partySelect}
+          // placeholder="Select Seller Firm Name"
+          value={selectSellerParty?.name || ""}
+          onChange={sellerPartySelect}
         >
+          <option value="" disabled>
+            Select Seller Firm Name
+          </option>
           {parties.map((party, index) => (
             <option key={index} value={party.name}>
               {party.name}
             </option>
           ))}
         </select>
-
+        <hr />
+        <Text>Buyer Information</Text>
+        <select
+          // placeholder="Select Buyer Firm Name"
+          value={selectBuyerParty?.name || ""}
+          onChange={buyerPartySelect}
+        >
+          <option value="" disabled>
+            Select Buyer Firm Name
+          </option>
+          {parties.map((party, index) => (
+            <option key={index} value={party.name}>
+              {party.name}
+            </option>
+          ))}
+        </select>
         <br />
       </FormControl>
 
       <hr />
-        <h2><Text>Seller Information</Text></h2>    
-        
+      <h2>
+        <Text>Seller Information</Text>
+      </h2>
 
-        {selectParty && (
+      {selectSellerParty && (
+        <>
+          <h4>
+            <Text>Consignee Name: {selectSellerParty.name}</Text>
+          </h4>
+          <Text>Address: {selectSellerParty.address}</Text>
+          <Text>City: {selectSellerParty.city}</Text>
+          <Text>State: {selectSellerParty.state}</Text>
+          <Text>GST No: {selectSellerParty.gstNo}</Text>
+        </>
+      )}
+
+      <hr />
+      <h2>Buyer Information</h2>
+
+      {selectBuyerParty && (
         <>
           <hr />
-          <h4><Text>Consignee Name : {selectParty.name}</Text></h4>
-          <Text>Address: {selectParty.address}</Text>
-          <Text>City: {selectParty.city}</Text>
-          <Text>State: {selectParty.state}</Text>
-          <Text>GST No: {selectParty.gstNo}</Text>
+          <h4>
+            <Text>Consignee Name: {selectBuyerParty.name}</Text>
+          </h4>
+          <Text>Address: {selectBuyerParty.address}</Text>
+          <Text>City: {selectBuyerParty.city}</Text>
+          <Text>State: {selectBuyerParty.state}</Text>
+          <Text>GST No: {selectBuyerParty.gstNo}</Text>
         </>
-        )}
+      )}
     </>
   );
 };
 
 export default InvoiceForm;
-
-
